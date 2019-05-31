3645ec0b75f711bf4314ca99539bfa56de66e0d5
mintsuki <mintsuki@protonmail.com>
2019-05-31 06:23:23 +0200

    Added libc.h

---
+++ lib/libc.c
@@ -0,0 +1,113 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <lib/libc.h>
+
+void *memcpy(void *dest, const void *src, size_t count) {
+    size_t i = 0;
+
+    uint8_t *dest2 = dest;
+    const uint8_t *src2 = src;
+
+    for (i = 0; i < count; i++) {
+        dest2[i] = src2[i];
+    }
+
+    return dest;
+}
+
+void *memset(void *s, int c, size_t count) {
+    uint8_t *p = s, *end = p + count;
+    for (; p != end; p++) {
+        *p = (uint8_t)c;
+    }
+
+    return s;
+}
+
+void *memmove(void *dest, const void *src, size_t count) {
+    size_t i = 0;
+
+    uint8_t *dest2 = dest;
+    const uint8_t *src2 = src;
+
+    if (src > dest) {
+        for (i = 0; i < count; i++) {
+            dest2[i] = src2[i];
+        }
+    } else if (src < dest) {
+        for (i = count; i > 0; i--) {
+            dest2[i - 1] = src2[i - 1];
+        }
+    }
+
+    return dest;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+    const uint8_t *a = s1;
+    const uint8_t *b = s2;
+
+    for (size_t i = 0; i < n; i++) {
+        if (a[i] < b[i]) {
+            return -1;
+        } else if (a[i] > b[i]) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+char *strchrnul(const char *s, int c) {
+    while (*s)
+        if ((*s++) == c)
+            break;
+    return (char *)s;
+}
+
+char *strcpy(char *dest, const char *src) {
+    size_t i = 0;
+
+    for (i = 0; src[i]; i++)
+        dest[i] = src[i];
+
+    dest[i] = 0;
+
+    return dest;
+}
+
+char *strncpy(char *dest, const char *src, size_t cnt) {
+    size_t i = 0;
+
+    for (i = 0; i < cnt; i++)
+        dest[i] = src[i];
+
+    return dest;
+}
+
+int strcmp(const char *dst, const char *src) {
+    size_t i;
+
+    for (i = 0; dst[i] == src[i]; i++) {
+        if ((!dst[i]) && (!src[i])) return 0;
+    }
+
+    return 1;
+}
+
+int strncmp(const char *dst, const char *src, size_t count) {
+    size_t i;
+
+    for (i = 0; i < count; i++)
+        if (dst[i] != src[i]) return 1;
+
+    return 0;
+}
+
+size_t strlen(const char *str) {
+    size_t len;
+
+    for (len = 0; str[len]; len++);
+
+    return len;
+}
---
+++ lib/libc.h
@@ -0,0 +1,17 @@
+#ifndef __LIBC_H__
+#define __LIBC_H__
+
+#include <stddef.h>
+
+void *memset(void *, int, size_t);
+void *memcpy(void *, const void *, size_t);
+int memcmp(const void *, const void *, size_t);
+void *memmove(void *, const void *, size_t);
+char *strchrnul(const char *, int);
+char *strcpy(char *, const char *);
+char *strncpy(char *, const char *, size_t);
+size_t strlen(const char *);
+int strcmp(const char *, const char *);
+int strncmp(const char *, const char *, size_t);
+
+#endif

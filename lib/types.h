d624a35427815494e7bc46da066a1997b4ec24ce
mintsuki <mintsuki@protonmail.com>
2020-01-21 11:42:17 +0100

    Add a .bss section to linker script

---
+++ lib/types.h
@@ -0,0 +1,6 @@
+#ifndef __LIB_TYPES_H__
+#define __LIB_TYPES_H__
+
+typedef void *symbol[];
+
+#endif
--- main.c
+++ main.c
@@ -8,12 +8,21 @@ asm (
 #include <drivers/vga_textmode.h>
 #include <lib/real.h>
 #include <lib/print.h>
+#include <lib/types.h>
+
+extern symbol bss_begin;
+extern symbol bss_end;
 
 void main(int boot_drive) {
+    // Zero out .bss section
+    for (uint8_t *p = bss_begin; p < bss_end; p++)
+        *p = 0;
+
     init_vga_textmode();
     print("qLoader 2\n\n");
-    print("=> Boot drive: %x\n", boot_drive);
-    print("\n");
+
+    print("=> Boot drive: %x\n\n", boot_drive);
+
     for (;;) {
         struct rm_regs r = {0};
         rm_int(0x16, &r, &r);    // Real mode interrupt 16h

f7b0f240d369862135b341fbc7aee99ac26a6e89
mintsuki <mintsuki@protonmail.com>
2020-01-21 23:45:13 +0100

    Add make toolchain script and reorganise source tree

--- Makefile
+++ Makefile
@@ -1,43 +1,7 @@
-CC = gcc
-LD = ld
-
-CFLAGS = -O2 -pipe -Wall -Wextra
-
-INTERNAL_CFLAGS = \
-	-m32 \
-	-ffreestanding \
-	-nostdlib \
-	-masm=intel \
-	-fno-pic \
-	-mno-sse \
-	-mno-sse2 \
-	-ffreestanding \
-	-fno-stack-protector \
-	-I.
-
-LDFLAGS =
-
-INTERNAL_LDFLAGS = \
-	-m elf_i386 \
-	-nostdlib \
-	-Tlinker.ld
-
 .PHONY: all clean
 
-C_FILES := $(shell find ./ -type f -name '*.c')
-OBJ := $(C_FILES:.c=.o)
-
-all: qloader2.bin
-
-qloader2.bin: bootsect/bootsect.bin $(OBJ)
-	$(LD) $(LDFLAGS) $(INTERNAL_LDFLAGS) $(OBJ) -o stage2.bin
-	cat bootsect/bootsect.bin stage2.bin > $@
-
-bootsect/bootsect.bin: bootsect/bootsect.asm
-	cd bootsect && nasm bootsect.asm -fbin -o bootsect.bin
-
-%.o: %.c
-	$(CC) $(CFLAGS) $(INTERNAL_CFLAGS) -c $< -o $@
+all:
+	$(MAKE) -C src all
 
 clean:
-	rm -f $(OBJ) bootsect/bootsect.bin
+	$(MAKE) -C src clean
---
+++ toolchain/make_toolchain.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+PREFIX="$(pwd)"
+TARGET=i386-elf
+BINUTILSVERSION=2.33.1
+GCCVERSION=9.2.0
+
+if [ -z "$MAKEFLAGS" ]; then
+	MAKEFLAGS="$1"
+fi
+export MAKEFLAGS
+
+export PATH="$PREFIX/bin:$PATH"
+
+if [ -x "$(command -v gmake)" ]; then
+    mkdir -p "$PREFIX/bin"
+    cat <<EOF >"$PREFIX/bin/make"
+#!/usr/bin/env sh
+gmake "\$@"
+EOF
+    chmod +x "$PREFIX/bin/make"
+fi
+
+mkdir -p build
+cd build
+
+if [ ! -f binutils-$BINUTILSVERSION.tar.gz ]; then
+    wget https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILSVERSION.tar.gz
+fi
+if [ ! -f gcc-$GCCVERSION.tar.gz ]; then
+    wget https://ftp.gnu.org/gnu/gcc/gcc-$GCCVERSION/gcc-$GCCVERSION.tar.gz
+fi
+
+tar -xf binutils-$BINUTILSVERSION.tar.gz
+tar -xf gcc-$GCCVERSION.tar.gz
+
+rm -rf build-gcc build-binutils
+
+mkdir build-binutils
+cd build-binutils
+../binutils-$BINUTILSVERSION/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+cd ..
+
+cd gcc-$GCCVERSION
+contrib/download_prerequisites
+cd ..
+mkdir build-gcc
+cd build-gcc
+../gcc-$GCCVERSION/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc

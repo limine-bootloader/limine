2b6cf6296c43392a3343b73ede14158fd19a1012
mintsuki <mintsuki@protonmail.com>
2020-04-24 17:28:19 +0200

    Fix bug where config file values with a colon were interpreted as entries (2)

--- src/lib/config.c
+++ src/lib/config.c
@@ -40,6 +40,8 @@ int config_get_entry_name(char *ret, size_t index, size_t limit) {
             p++;
         }
         p++;
+        if (*(p - 2) != '\n')
+            i--;
     }
 
     size_t i;
@@ -69,8 +71,12 @@ int config_set_entry(size_t index) {
 
     config_addr = p;
 
+cont:
     while (*p != ':' && *p)
         p++;
+
+    if (*p && *(p - 1) != '\n')
+        goto cont;
 
     *p = 0;
 

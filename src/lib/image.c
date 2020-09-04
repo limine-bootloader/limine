8305266010c18a6ea3e87373008675c17d63d569
ethan <55000224+ethan4984@users.noreply.github.com>
2020-09-04 03:03:47 -0600

    Update image.c

--- src/lib/image.c
+++ src/lib/image.c
@@ -19,8 +19,8 @@ int get_image_info(background_image_info_t *image_info) {
                               "BACKGROUND_PATH", (char*)&path
                           };
 
-    for(uint32_t i = 0; i < SIZEOF_ARRAY(dependences); i += 2) {
-        if(!config_get_value(dependences[i + 1], 0, 128, dependences[i])) {
+    for (uint32_t i = 0; i < SIZEOF_ARRAY(dependences); i += 2) {
+        if (!config_get_value(dependences[i + 1], 0, 128, dependences[i])) {
             print("%s not specified\n", dependences[i]); 
             return 0;
         }
@@ -37,14 +37,14 @@ int get_image_info(background_image_info_t *image_info) {
 void draw_image() {
     background_image_info_t image_info; 
 
-    if(!get_image_info(&image_info)) 
+    if (!get_image_info(&image_info)) 
         return;
 
-    for(uint32_t i = 0; i < SIZEOF_ARRAY(file_extensions); i++) { 
-        if(!strcmp(file_extensions[i], image_info.path + (strlen(image_info.path) - strlen(file_extensions[i])))) {
+    for (uint32_t i = 0; i < SIZEOF_ARRAY(file_extensions); i++) { 
+        if (!strcmp(file_extensions[i], image_info.path + (strlen(image_info.path) - strlen(file_extensions[i])))) {
             struct file_handle fd;
 
-            if(fopen(&fd, image_info.drive, image_info.part, image_info.path)) {
+            if (fopen(&fd, image_info.drive, image_info.part, image_info.path)) {
                 print("%s could not be opened", image_info.path);
                 return;
             }
@@ -52,4 +52,4 @@ void draw_image() {
             image_handler[i](fd);
         }
     }
-} 
+}

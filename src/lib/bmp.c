c3cd79769e3a1d3cb064c2bc149305515007599f
ethan <55000224+ethan4984@users.noreply.github.com>
2020-09-04 03:02:31 -0600

    Update bmp.c

--- src/lib/bmp.c
+++ src/lib/bmp.c
@@ -5,21 +5,28 @@
 #include <fs/file.h>
 #include <lib/bmp.h>
 
+uint32_t *bmp_image;
+
+static uint32_t bmp_get_pixel(int x, int y, uint32_t pitch) {
+    return bmp_image[x + (pitch / sizeof(uint32_t)) * y];
+}
+
 void draw_bmp(struct file_handle fd) {
     bmp_file_hdr_t bmp_file_hdr;
     fread(&fd, &bmp_file_hdr, 0, sizeof(bmp_file_hdr_t));
 
+    bmp_image = (uint32_t*)ext_mem_balloc(bmp_file_hdr.bf_size);
+    fread(&fd, bmp_image, bmp_file_hdr.bf_offset, bmp_file_hdr.bf_size);
+
     uint32_t pitch = bmp_file_hdr.bi_width * (bmp_file_hdr.bi_bpp / 8);
 
-    uint32_t *data = balloc(pitch); 
-    uint32_t cnt = bmp_file_hdr.bf_offset;
+    uint32_t x = 0, y = bmp_file_hdr.bi_height;
 
-    for (int i = bmp_file_hdr.bi_height - 1; i > -1; i--) {
-        fread(&fd, data, cnt, pitch);
-        cnt += pitch;
-        for (uint32_t j = 0; j < bmp_file_hdr.bi_width; j++)
-            vbe_plot_px(j, i, data[j]);
+    for (uint32_t i = 0; i < bmp_file_hdr.bi_height; i++) {
+        for (uint32_t j = 0; j < bmp_file_hdr.bi_width; j++) {
+            vbe_plot_px(x++, y, bmp_get_pixel(j, i, pitch));
+        }
+        x = 0;
+        y--;
     }
-
-    brewind(pitch);
 }

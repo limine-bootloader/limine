limine-bootloader/limine

commit e721c3c814736b9052d050b58830f970ceb09c99

Reorganise pmm code

stage2/fs/ext2.c
@@ -5,6 +5,7 @@
 #include <drivers/disk.h>
 #include <lib/libc.h>
 #include <lib/blib.h>
+#include <mm/pmm.h>
 
 /* EXT2 Filesystem States */
 #define EXT2_FS_UNRECOVERABLE_ERRORS 3
@@ -125,7 +126,7 @@ static int ext2_parse_dirent(struct ext2_dir_entry *dir, struct ext2_file_handle
     if (*path == '/')
         path++;
 
-    struct ext2_inode *current_inode = balloc(sizeof(struct ext2_inode));
+    struct ext2_inode *current_inode = conv_mem_alloc(sizeof(struct ext2_inode));
     *current_inode = fd->root_inode;
 
     bool escape = false;
@@ -148,7 +149,7 @@ next:
                    fd->drive, &fd->part);
 
         // name read
-        char *name = balloc(dir->name_len + 1);
+        char *name = conv_mem_alloc(dir->name_len + 1);
 
         memset(name, 0, dir->name_len + 1);
         inode_read(name, i + sizeof(struct ext2_dir_entry), dir->name_len,
@@ -156,11 +157,11 @@ next:
 
         int r = strcmp(token, name);
 
-        brewind(dir->name_len);
+        conv_mem_rewind();
 
         if (!r) {
             if (escape) {
-                brewind(sizeof(struct ext2_inode));
+                conv_mem_rewind();
                 return 0;
             } else {
                 // update the current inode
@@ -172,7 +173,7 @@ next:
         i += dir->rec_len;
     }
 
-    brewind(sizeof(struct ext2_inode));
+    conv_mem_rewind();
     return -1;
 }
 

stage2/mm/pmm.c
@@ -1,9 +1,10 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <lib/memmap.h>
+#include <mm/pmm.h>
 #include <sys/e820.h>
 #include <lib/blib.h>
+#include <lib/libc.h>
 #include <lib/print.h>
 
 #define PAGE_SIZE   4096
@@ -157,13 +158,20 @@ void init_memmap(void) {
     sanitise_entries();
 }
 
-void *ext_mem_balloc(size_t count, uint32_t type) {
-    return ext_mem_balloc_aligned(count, 4, type);
+void *ext_mem_alloc(size_t count) {
+    return ext_mem_alloc_type(count, MEMMAP_BOOTLOADER_RECLAIMABLE);
 }
 
-// TODO: this basically only works for the 1st extended memory entry in the
-//       memmap and allocates until the first hole following it. Fix that.
-void *ext_mem_balloc_aligned(size_t count, size_t alignment, uint32_t type) {
+void *ext_mem_alloc_aligned(size_t count, size_t alignment) {
+    return ext_mem_alloc_aligned_type(count, alignment, MEMMAP_BOOTLOADER_RECLAIMABLE);
+}
+
+void *ext_mem_alloc_type(size_t count, uint32_t type) {
+    return ext_mem_alloc_aligned_type(count, 4, type);
+}
+
+// Allocate memory top down, hopefully without bumping into kernel or modules
+void *ext_mem_alloc_aligned_type(size_t count, size_t alignment, uint32_t type) {
     for (int i = memmap_entries - 1; i >= 0; i--) {
         if (memmap[i].type != 1)
             continue;
@@ -189,7 +197,12 @@ void *ext_mem_balloc_aligned(size_t count, size_t alignment, uint32_t type) {
         int64_t aligned_length = entry_top - alloc_base;
         memmap_alloc_range((uint64_t)alloc_base, (uint64_t)aligned_length, type);
 
-        return (void *)(size_t)alloc_base;
+        void *ret = (void *)(size_t)alloc_base;
+
+        // Zero out allocated space
+        memset(ret, 0, count);
+
+        return ret;
     }
 
     panic("High memory allocator: Out of memory");
@@ -245,4 +258,46 @@ void memmap_alloc_range(uint64_t base, uint64_t length, uint32_t type) {
     }
 
     panic("Out of memory");
+}
+
+extern symbol bss_end;
+static size_t bump_allocator_base = (size_t)bss_end;
+static size_t bump_allocator_limit = 0;
+
+void conv_mem_rewind(void) {
+    size_t *old_base = (size_t *)(bump_allocator_base - sizeof(size_t));
+    bump_allocator_base = *old_base;
+}
+
+void *conv_mem_alloc(size_t count) {
+    return conv_mem_alloc_aligned(count, 4);
+}
+
+void *conv_mem_alloc_aligned(size_t count, size_t alignment) {
+    if (!bump_allocator_limit) {
+        // The balloc limit is the beginning of the GDT
+        struct {
+            uint16_t limit;
+            uint32_t ptr;
+        } __attribute__((packed)) gdtr;
+        asm volatile ("sgdt %0" :: "m"(gdtr));
+        bump_allocator_limit = gdtr.ptr;
+    }
+
+    size_t new_base = ALIGN_UP(bump_allocator_base, alignment);
+    void *ret = (void *)new_base;
+
+    size_t *old_base = (size_t *)(new_base + count);
+    new_base += count + sizeof(size_t);
+
+    if (new_base >= bump_allocator_limit)
+        panic("Memory allocation failed");
+
+    *old_base = bump_allocator_base;
+    bump_allocator_base = new_base;
+
+    // Zero out allocated space
+    memset(ret, 0, count);
+
+    return ret;
 }

stage2/mm/pmm.h
@@ -1,0 +1,29 @@
+#ifndef __MM__PMM_H__
+#define __MM__PMM_H__
+
+#include <stdint.h>
+#include <sys/e820.h>
+
+#define MEMMAP_USABLE                 1
+#define MEMMAP_RESERVED               2
+#define MEMMAP_ACPI_RECLAIMABLE       3
+#define MEMMAP_ACPI_NVS               4
+#define MEMMAP_BAD_MEMORY             5
+#define MEMMAP_BOOTLOADER_RECLAIMABLE 0x1000
+#define MEMMAP_KERNEL_AND_MODULES     0x1001
+
+void init_memmap(void);
+struct e820_entry_t *get_memmap(size_t *entries);
+void print_memmap(struct e820_entry_t *mm, size_t size);
+void memmap_alloc_range(uint64_t base, uint64_t length, uint32_t type);
+
+void *ext_mem_alloc(size_t count);
+void *ext_mem_alloc_type(size_t count, uint32_t type);
+void *ext_mem_alloc_aligned(size_t count, size_t alignment);
+void *ext_mem_alloc_aligned_type(size_t count, size_t alignment, uint32_t type);
+
+void conv_mem_rewind(void);
+void *conv_mem_alloc(size_t count);
+void *conv_mem_alloc_aligned(size_t count, size_t alignment);
+
+#endif

stage2/sys/smp.c
@@ -8,6 +8,7 @@
 #include <sys/smp.h>
 #include <sys/lapic.h>
 #include <mm/vmm64.h>
+#include <mm/pmm.h>
 
 struct madt {
     struct sdt;
@@ -88,7 +89,7 @@ struct smp_information *init_smp(size_t   *cpu_count,
     struct gdtr gdtr;
     asm volatile ("sgdt %0" :: "m"(gdtr));
 
-    struct smp_information *ret = balloc_aligned(0, 1);
+    struct smp_information *ret = conv_mem_alloc_aligned(0, 1);
     *cpu_count = 0;
 
     // Parse the MADT entries
@@ -104,7 +105,7 @@ struct smp_information *init_smp(size_t   *cpu_count,
                 struct madt_lapic *lapic = (void *)madt_ptr;
 
                 struct smp_information *info_struct =
-                        balloc_aligned(sizeof(struct smp_information), 1);
+                        conv_mem_alloc_aligned(sizeof(struct smp_information), 1);
 
                 info_struct->acpi_processor_uid = lapic->acpi_processor_uid;
                 info_struct->lapic_id           = lapic->lapic_id;
@@ -125,7 +126,7 @@ struct smp_information *init_smp(size_t   *cpu_count,
                 if (!smp_start_ap(lapic->lapic_id, &gdtr, info_struct,
                                   longmode, lv5, (uint32_t)pagemap.top_level)) {
                     print("smp: FAILED to bring-up AP\n");
-                    brewind(sizeof(struct smp_information));
+                    conv_mem_rewind();
                     continue;
                 }
 

stage2/drivers/disk.c
@@ -6,6 +6,7 @@
 #include <lib/blib.h>
 #include <lib/part.h>
 #include <lib/print.h>
+#include <mm/pmm.h>
 
 #define SECTOR_SIZE 512
 #define BLOCK_SIZE_IN_SECTORS 16
@@ -29,7 +30,7 @@ static int cache_block(int drive, uint64_t block) {
         return 0;
 
     if (!cache)
-        cache = balloc_aligned(BLOCK_SIZE, 16);
+        cache = conv_mem_alloc_aligned(BLOCK_SIZE, 16);
 
     dap.segment = rm_seg(cache);
     dap.offset  = rm_off(cache);

stage2/drivers/vbe.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <drivers/vbe.h>
-#include <lib/memmap.h>
 #include <lib/libc.h>
 #include <lib/blib.h>
 #include <lib/real.h>
 #include <lib/print.h>
+#include <mm/pmm.h>
 
 #define VGA_FONT_WIDTH  8
 #define VGA_FONT_HEIGHT 16
@@ -22,7 +22,7 @@ static void vga_font_retrieve(void) {
     r.ebx = 0x0600;
     rm_int(0x10, &r, &r);
 
-    vga_font = ext_mem_balloc(VGA_FONT_MAX, MEMMAP_BOOTLOADER_RECLAIMABLE);
+    vga_font = ext_mem_alloc(VGA_FONT_MAX);
 
     memcpy(vga_font, (void *)rm_desegment(r.es, r.ebp), VGA_FONT_MAX);
 }
@@ -222,7 +222,7 @@ void vbe_tty_init(int *_rows, int *_cols) {
     vga_font_retrieve();
     *_cols = cols = vbe_width / VGA_FONT_WIDTH;
     *_rows = rows = vbe_height / VGA_FONT_HEIGHT;
-    grid = ext_mem_balloc(rows * cols * sizeof(struct vbe_char), MEMMAP_BOOTLOADER_RECLAIMABLE);
+    grid = ext_mem_alloc(rows * cols * sizeof(struct vbe_char));
     vbe_clear(true);
 }
 

stage2/fs/echfs.c
@@ -5,6 +5,7 @@
 #include <lib/print.h>
 #include <drivers/disk.h>
 #include <stdbool.h>
+#include <mm/pmm.h>
 
 struct echfs_identity_table {
     uint8_t  jmp[4];
@@ -123,7 +124,7 @@ found:;
     // Load the allocation map.
     uint64_t file_block_count = DIV_ROUNDUP(ret->dir_entry.size, ret->block_size);
 
-    ret->alloc_map = balloc(file_block_count * sizeof(uint64_t));
+    ret->alloc_map = conv_mem_alloc(file_block_count * sizeof(uint64_t));
 
     ret->alloc_map[0] = ret->dir_entry.payload;
     for (uint64_t i = 1; i < file_block_count; i++) {

commit 0dc73b35299e7d0e01f17b6158d17c1921826e82

Fix conv_mem_alloc bug introduced by e721c3c

stage2/fs/ext2.c
@@ -220,11 +220,11 @@ next:
 
         int r = strcmp(token, name);
 
-        conv_mem_rewind();
+        conv_mem_rewind(dir->name_len + 1);
 
         if (!r) {
             if (escape) {
-                conv_mem_rewind();
+                conv_mem_rewind(sizeof(struct ext2_inode));
                 return 0;
             } else {
                 // update the current inode
@@ -236,7 +236,7 @@ next:
         i += dir->rec_len;
     }
 
-    conv_mem_rewind();
+    conv_mem_rewind(sizeof(struct ext2_inode));
     return -1;
 }
 

stage2/mm/pmm.c
@@ -264,9 +264,8 @@ extern symbol bss_end;
 static size_t bump_allocator_base = (size_t)bss_end;
 static size_t bump_allocator_limit = 0;
 
-void conv_mem_rewind(void) {
-    size_t *old_base = (size_t *)(bump_allocator_base - sizeof(size_t));
-    bump_allocator_base = *old_base;
+void conv_mem_rewind(size_t count) {
+    bump_allocator_base -= count;
 }
 
 void *conv_mem_alloc(size_t count) {
@@ -286,14 +285,9 @@ void *conv_mem_alloc_aligned(size_t count, size_t alignment) {
 
     size_t new_base = ALIGN_UP(bump_allocator_base, alignment);
     void *ret = (void *)new_base;
-
-    size_t *old_base = (size_t *)(new_base + count);
-    new_base += count + sizeof(size_t);
-
+    new_base += count;
     if (new_base >= bump_allocator_limit)
         panic("Memory allocation failed");
-
-    *old_base = bump_allocator_base;
     bump_allocator_base = new_base;
 
     // Zero out allocated space

stage2/mm/pmm.h
@@ -22,7 +22,7 @@ void *ext_mem_alloc_type(size_t count, uint32_t type);
 void *ext_mem_alloc_aligned(size_t count, size_t alignment);
 void *ext_mem_alloc_aligned_type(size_t count, size_t alignment, uint32_t type);
 
-void conv_mem_rewind(void);
+void conv_mem_rewind(size_t count);
 void *conv_mem_alloc(size_t count);
 void *conv_mem_alloc_aligned(size_t count, size_t alignment);
 

stage2/sys/smp.c
@@ -126,7 +126,7 @@ struct smp_information *init_smp(size_t   *cpu_count,
                 if (!smp_start_ap(lapic->lapic_id, &gdtr, info_struct,
                                   longmode, lv5, (uint32_t)pagemap.top_level)) {
                     print("smp: FAILED to bring-up AP\n");
-                    conv_mem_rewind();
+                    conv_mem_rewind(sizeof(struct smp_information));
                     continue;
                 }
 
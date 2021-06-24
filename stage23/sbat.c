60e1b43f6665e5cf3890c0b964bfdfffc87699b9
mintsuki <mintsuki@protonmail.com>
2021-06-25 01:23:26 +0200

    secureboot: Add .sbat section

---
+++ stage23/sbat.c
@@ -0,0 +1,9 @@
+#if defined (uefi)
+
+asm (
+    ".section .sbat\n\t"
+    ".ascii \"sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md\\n\"\n\t"
+    ".ascii \"limine,1,Limine,limine," LIMINE_VERSION ",https://limine-bootloader.org\\n\"\n\t"
+);
+
+#endif

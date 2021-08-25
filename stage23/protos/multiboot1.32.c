0562c8ef700ea9824f6b615bbf868e86db67dbf0
mintsuki <mintsuki@protonmail.com>
2021-08-26 01:41:13 +0200

    multiboot1: When on BIOS, set IDTR to BIOS defaults before handoff

--- stage23/protos/multiboot1.32.c
+++ stage23/protos/multiboot1.32.c
@@ -2,9 +2,26 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <mm/vmm.h>
+#if bios == 1
+#  include <sys/idt.h>
+#endif
 
 __attribute__((noreturn)) void multiboot1_spinup_32(
                  uint32_t entry_point, uint32_t multiboot1_info) {
+#if bios == 1
+    struct idtr idtr;
+
+    idtr.limit = 0x3ff;
+    idtr.ptr = 0;
+
+    asm volatile (
+        "lidt %0"
+        :
+        : "m" (idtr)
+        : "memory"
+    );
+#endif
+
     asm volatile (
         "cld\n\t"
 

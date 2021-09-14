ab19d6ccad5eee408c52e7d8e0775662c9d8c014
mintsuki <mintsuki@protonmail.com>
2021-09-14 18:35:42 +0200

    menu: Clear line before reprinting timeout message

--- stage23/menu.c
+++ stage23/menu.c
@@ -634,7 +634,7 @@ refresh:
         for (size_t i = timeout; i; i--) {
             set_cursor_pos(0, term_rows - 1);
             scroll_disable();
-            print("\e[32mBooting automatically in \e[92m%u\e[32m, press any key to stop the countdown...\e[0m", i);
+            print("\e[2K\e[32mBooting automatically in \e[92m%u\e[32m, press any key to stop the countdown...\e[0m", i);
             scroll_enable();
             term_double_buffer_flush();
             if ((c = pit_sleep_and_quit_on_keypress(1))) {

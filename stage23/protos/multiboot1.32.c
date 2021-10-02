403f18da523af95e3bde405489b7afff688f11cf
mintsuki <mintsuki@protonmail.com>
2021-10-02 22:17:22 +0200

    multiboot: Zero out all undefined GPRs before handoff

--- stage23/protos/multiboot1.32.c
+++ stage23/protos/multiboot1.32.c
@@ -25,15 +25,19 @@ __attribute__((noreturn)) void multiboot1_spinup_32(
     asm volatile (
         "cld\n\t"
 
-        "pushl $0x18\n\t"
-        "pushl %%edi\n\t"
+        "push %2\n\t"
 
-        "movl $0x2BADB002, %%eax\n\t"
+        "xor %%ecx, %%ecx\n\t"
+        "xor %%edx, %%edx\n\t"
+        "xor %%esi, %%esi\n\t"
+        "xor %%edi, %%edi\n\t"
+        "xor %%ebp, %%ebp\n\t"
 
-        "lret\n\t"
+        "ret\n\t"
         :
-        : "D" (entry_point),
-          "b" (multiboot1_info)
+        : "a" (0x2badb002),
+          "b" (multiboot1_info),
+          "r" (entry_point)
         : "memory"
     );
 

dea6b930dc0f0fc65aff7d03d5427b3a88198310
mintsuki <mintsuki@protonmail.com>
2021-10-31 19:09:21 +0100

    stivale2: Remove useless arguments to load function

--- stage23/protos/stivale2.h
+++ stage23/protos/stivale2.h
@@ -1,8 +1,6 @@
 #ifndef __PROTOS__STIVALE2_H__
 #define __PROTOS__STIVALE2_H__
 
-#include <stdbool.h>
-
-void stivale2_load(char *config, char *cmdline, bool pxe, void *efi_system_table);
+void stivale2_load(char *config, char *cmdline);
 
 #endif

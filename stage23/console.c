3bf2635d90bc101d895832bdfec136c34d2d79ba
mintsuki <mintsuki@protonmail.com>
2021-10-31 20:33:47 +0100

    console: Remove license command

--- stage23/console.c
+++ stage23/console.c
@@ -14,7 +14,6 @@ static void console_help(void) {
         "exit      -- Return to boot menu.\n"
         "version   -- Print version.\n"
         "copyright -- Print copyright.\n"
-        "license   -- Print license.\n"
         "help      -- Print this help message.\n"
     );
 }
@@ -41,8 +40,7 @@ void console(void) {
         } else if (strcmp(prompt, "copyright") == 0) {
             print(LIMINE_COPYRIGHT "\n");
             print("Limine is distributed under the terms of the BSD-2-Clause license.\n");
-        } else if (strcmp(prompt, "license") == 0) {
-            print("%s", bsd_2_clause);
+            print("There is ABSOLUTELY NO WARRANTY, to the extent permitted by law.\n");
         } else if (*prompt != 0) {
             print("Invalid command: `%s`.\n", prompt);
         }

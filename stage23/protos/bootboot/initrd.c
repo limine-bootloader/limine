e0fefcf21c440f3675b0278e37e904a6ca3fb6df
mintsuki <mintsuki@protonmail.com>
2021-11-10 12:25:45 +0100

    bootboot: Remove uselessness and compliance fixes (see #124)

--- stage23/protos/bootboot/initrd.c
+++ stage23/protos/bootboot/initrd.c
@@ -1,9 +1,39 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <protos/bootboot/initrd.h>
 #include <lib/print.h>
 #include <lib/libc.h>
 #include <lib/blib.h>
+
+struct initrd_file bruteforce_kernel(struct initrd_file file) {
+    for (size_t i = 0; i < file.size; i++) {
+        if (memcmp(file.data + i, "\177ELF", 4) == 0) {
+            printv("bootboot: using bruteforced kernel at initrd offset %X\n", file.data + i);
+            return (struct initrd_file){
+                .size = file.size - i,
+                .data = file.data + i
+            };
+        }
+    }
+    return (struct initrd_file){0};
+}
+
+bool known_initrd_format(struct initrd_file file) {
+    if (file.size >= 5 && file.data[4] == 0xbf) {
+        return true;
+    }
+
+    if (file.size >= 5 && memcmp("07070", file.data, 5) == 0) {
+        return true;
+    }
+
+    if (file.size >= 262 && memcmp("ustar", file.data + 257, 5) == 0) {
+        return true;
+    }
+
+    return false;
+}
 
 INITRD_HANDLER(jamesm);
 INITRD_HANDLER(ustar);
@@ -11,43 +41,16 @@ INITRD_HANDLER(cpio);
 
 INITRD_HANDLER(auto) {
     if (file.size >= 5 && file.data[4] == 0xbf) {
-        struct initrd_file jamesm_attempt = initrd_open_jamesm(file, path);
-        if (jamesm_attempt.data) {
-            printv("bootboot: jamesm matched when reading file `%s`\n", path);
-            return jamesm_attempt;
-        }
-        panic("bootboot: cannot read file `%s`: no such file or directory", path);
+        return initrd_open_jamesm(file, path);
     }
 
     if (file.size >= 5 && memcmp("07070", file.data, 5) == 0) {
-        struct initrd_file cpio_attempt = initrd_open_cpio(file, path);
-        if (cpio_attempt.data) {
-            printv("bootboot: cpio matched when reading file `%s`\n", path);
-            return cpio_attempt;
-        }
-        panic("bootboot: cannot read file `%s`: no such file or directory", path);
+        return initrd_open_cpio(file, path);
     }
 
     if (file.size >= 262 && memcmp("ustar", file.data + 257, 5) == 0) {
-        struct initrd_file ustar_attempt = initrd_open_ustar(file, path);
-        if (ustar_attempt.data) {
-            printv("bootboot: ustar matched when reading file `%s`\n", path);
-            return ustar_attempt;
-        }
-        panic("bootboot: cannot read file `%s`: no such file or directory", path);
+        return initrd_open_ustar(file, path);
     }
 
-    if (strcmp("sys/core", path) == 0) {
-        for (size_t i = 0; i < file.size; i += 4) {
-            if (memcmp(file.data + i, "\177ELF", 4) == 0) {
-                printv("bootboot: using ELF as initrd to open sys/core\n");
-                return (struct initrd_file){
-                    .size = file.size - i,
-                    .data = file.data + i
-                };
-            }
-        }
-    }
-
-    panic("bootboot: cannot read file `%s`: cannot detect initrd type (only ustar, cpio and jamesm is supported).", path);
+    return (struct initrd_file){0};
 }

2db458511bbd2eb896eeca216f814b3653aa0f55
lukflug <lukflug@googlemail.com>
2021-12-10 20:30:46 +0100

    Added crude, but dependencyless (aside from legacy int 10h) fallback print function to panic

--- stage23/lib/panic.s2.c
+++ stage23/lib/panic.s2.c
@@ -10,12 +10,55 @@
 #include <lib/term.h>
 #include <mm/pmm.h>
 
+static const char* recursive_panic = 0;
+
+void fallback_print (const char* string) {
+    int i;
+    struct rm_regs r = {0};
+    for (i=0; string[i]; i++) {
+        r.eax = 0x0E00|string[i];
+        rm_int(0x10,&r,&r);
+    }
+}
+
 __attribute__((noreturn)) void panic(const char *fmt, ...) {
+
     va_list args;
 
     va_start(args, fmt);
 
     quiet = false;
+
+    if (recursive_panic) {
+#if bios == 1
+        struct rm_regs r = {0};
+        rm_int(0x11,&r,&r);
+        switch ((r.eax>>4)&3) {
+            case 0:
+                r.eax = 3;
+                break;
+            case 1:
+                r.eax = 1;
+                break;
+            case 2:
+                r.eax = 3;
+                break;
+            case 3:
+                r.eax = 7;
+                break;
+        }
+        rm_int(0x10,&r,&r);
+        fallback_print("RECURSIVE PANIC: \r\n");
+        fallback_print("Original panic: ");
+        fallback_print(recursive_panic);
+        fallback_print("\r\nSubsequent panic: ");
+        fallback_print(fmt);
+        fallback_print("\r\nPress CTRL+ALT+DEL to reboot.");
+        rm_hcf();
+#endif
+    }
+
+    recursive_panic = fmt;
 
     if (term_backend == NOT_READY) {
 #if bios == 1

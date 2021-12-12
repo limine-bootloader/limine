760e1b20c4fa9caa965964496ea61c060e411858
mintsuki <mintsuki@protonmail.com>
2021-12-12 18:40:27 +0100

    misc: Unwind both .data and .bss

--- stage23/entry.s2.c
+++ stage23/entry.s2.c
@@ -81,16 +81,12 @@ void entry(uint8_t boot_drive, int boot_from) {
 
     term_notready();
 
-    {
     struct rm_regs r = {0};
     r.eax = 0x0003;
     rm_int(0x10, &r, &r);
 
-    current_video_mode = -1;
-
     outb(0x3d4, 0x0a);
     outb(0x3d5, 0x20);
-    }
 
     init_e820();
     init_memmap();

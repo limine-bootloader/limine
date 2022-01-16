4df4a1fde6099e4e35822234049066b02f77c4a2
mintsuki <mintsuki@protonmail.com>
2022-01-16 06:34:15 +0100

    build: Fix up version.sh

--- version.sh
+++ version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cat version 2>/dev/null || ( git describe --exact-match --tags `git log -n1 --pretty='%h'` 2>/dev/null || git log -n1 --pretty='%h' )
+[ -f version ] || ( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | xargs printf '%s'
+[ -f version ] && ( cat version 2>/dev/null ) | xargs printf '%s'

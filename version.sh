7b0c430fb5c0022e6307222d10f1625b467075f8
mintsuki <mintsuki@protonmail.com>
2022-01-21 02:24:36 +0100

    build: Misc portability improvements

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/sh
 
 [ -f version ] || ( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | xargs printf '%s'
 [ -f version ] && ( cat version 2>/dev/null ) | xargs printf '%s'

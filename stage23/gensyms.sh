d7b8c3cf8a6cedc425e8290aecaf5bb6f8f56926
mintsuki <mintsuki@protonmail.com>
2022-02-01 08:04:55 +0100

    build: Misc improvements

--- stage23/gensyms.sh
+++ stage23/gensyms.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-export LC_ALL=C
+LC_ALL=C
+export LC_ALL
 
 TMP0=$(mktemp)
 
@@ -25,24 +26,24 @@ TMP2=$(mktemp)
 TMP3=$(mktemp)
 TMP4=$(mktemp)
 
-$1 -t "$2" | ( sed '/[[:<:]]d[[:>:]]/d' 2>/dev/null || sed '/\bd\b/d' ) | sort > "$TMP1"
-grep "\.text" < "$TMP1" | cut -d' ' -f1 > "$TMP2"
-grep "\.text" < "$TMP1" | awk 'NF{ print $NF }' > "$TMP3"
+"$LIMINE_OBJDUMP" -t "$1" | ( "$SED" '/[[:<:]]d[[:>:]]/d' 2>/dev/null || "$SED" '/\bd\b/d' ) | sort > "$TMP1"
+"$GREP" "\.text" < "$TMP1" | cut -d' ' -f1 > "$TMP2"
+"$GREP" "\.text" < "$TMP1" | "$AWK" 'NF{ print $NF }' > "$TMP3"
 
-echo "section .$3_map" > "$TMP4"
-echo "global $3_map" >> "$TMP4"
-echo "$3_map:" >> "$TMP4"
+echo "section .$2_map" > "$TMP4"
+echo "global $2_map" >> "$TMP4"
+echo "$2_map:" >> "$TMP4"
 
-if [ "$4" = "32" ]; then
-    paste -d'$' "$TMP2" "$TMP3" | sed 's/^/dd 0x/g;s/$/", 0/g;s/\$/\
+if [ "$3" = "32" ]; then
+    paste -d'$' "$TMP2" "$TMP3" | "$SED" 's/^/dd 0x/g;s/$/", 0/g;s/\$/\
 db "/g' >> "$TMP4"
     echo "dd 0xffffffff" >> "$TMP4"
-    nasm -f elf32 "$TMP4" -o $3.map.o
-elif [ "$4" = "64" ]; then
-    paste -d'$' "$TMP2" "$TMP3" | sed 's/^/dq 0x/g;s/$/", 0/g;s/\$/\
+    nasm -f elf32 "$TMP4" -o $2.map.o
+elif [ "$3" = "64" ]; then
+    paste -d'$' "$TMP2" "$TMP3" | "$SED" 's/^/dq 0x/g;s/$/", 0/g;s/\$/\
 db "/g' >> "$TMP4"
     echo "dq 0xffffffffffffffff" >> "$TMP4"
-    nasm -f elf64 "$TMP4" -o $3.map.o
+    nasm -f elf64 "$TMP4" -o $2.map.o
 fi
 
 rm "$TMP1" "$TMP2" "$TMP3" "$TMP4"

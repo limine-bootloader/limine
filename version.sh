b138b1952e87a10a58da8825a457937d710ffa00
mintsuki <mintsuki@protonmail.com>
2022-02-10 14:47:38 +0100

    limine-install: Make hgen a shell script

--- version.sh
+++ version.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
+LC_ALL=C
+export LC_ALL
+
 [ -f version ] || ( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | sed 's/^v//g' | xargs printf '%s'
 [ -f version ] && ( cat version 2>/dev/null ) | xargs printf '%s'

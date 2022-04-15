98b54eaf2580bfa167d016f8e0bb025c36923b67
mintsuki <mintsuki@protonmail.com>
2022-04-15 16:13:08 +0200

    limine-deploy: Remove leading underscore from limine.hdd variable name

--- limine-deploy/hgen.sh
+++ limine-deploy/hgen.sh
@@ -6,7 +6,7 @@ LC_ALL=C
 export LC_ALL
 
 cat <<EOF
-const uint8_t _binary_limine_hdd_bin_data[] = {
+const uint8_t binary_limine_hdd_bin_data[] = {
 EOF
 
 od -v -An -t x1 <limine-hdd.bin | "$SED" '/^$/d;s/  */ /g;s/ *$//g;s/ /, 0x/g;s/^, /    /g;s/$/,/g'

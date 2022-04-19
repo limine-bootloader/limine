b9cde22b657c79200372b62200bf2d5e2d030412
mintsuki <mintsuki@protonmail.com>
2022-04-20 00:20:50 +0200

    build: Track specific reduced-gnu-efi commit

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,7 @@ cd "$srcdir"
 
 [ -d freestanding_headers ] || git clone https://github.com/mintsuki/freestanding_headers.git
 [ -d stivale ] || git clone https://github.com/stivale/stivale.git
-[ -d reduced-gnu-efi ] || git clone https://github.com/limine-bootloader/reduced-gnu-efi.git
+[ -d reduced-gnu-efi ] || ( git clone https://github.com/limine-bootloader/reduced-gnu-efi.git && cd reduced-gnu-efi && git checkout 456a1a82d2be9e5fd758ae819c3077140ac5aa99 )
 
 automake_libdir="$(automake --print-libdir)"
 

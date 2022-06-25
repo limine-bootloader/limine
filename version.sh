b8a3534a4141784e51609062f0b2e8003a85ffba
mintsuki <mintsuki@protonmail.com>
2022-06-25 10:47:07 +0200

    build: Unbreak version.sh

--- version.sh
+++ version.sh
@@ -3,4 +3,4 @@
 LC_ALL=C
 export LC_ALL
 
-git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' | sed 's/^v//g' | xargs printf '%s'
+( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | sed 's/^v//g' | xargs printf '%s'

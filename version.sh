c1c976179a2b4005551caca310b3ea147746aece
mintsuki <mintsuki@protonmail.com>
2022-07-02 14:01:18 +0200

    build: Fix version.sh for when it is ran from a different directory

--- version.sh
+++ version.sh
@@ -3,5 +3,10 @@
 LC_ALL=C
 export LC_ALL
 
+srcdir="$(dirname "$0")"
+test -z "$srcdir" && srcdir=.
+
+cd "$srcdir"
+
 [ -f version ] || ( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | sed 's/^v//g' | xargs printf '%s'
 [ -f version ] && ( cat version 2>/dev/null ) | xargs printf '%s'

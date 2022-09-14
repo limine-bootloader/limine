2b71eaedb6a525d0e3cab6254321251417ce220d
mintsuki <mintsuki@protonmail.com>
2022-09-14 15:39:52 +0200

    smp: Fix aarch64 issue introduced in 0f04f686

--- common/sys/smp.c
+++ common/sys/smp.c
@@ -502,9 +502,9 @@ static struct limine_smp_info *try_acpi_smp(size_t   *cpu_count,
 
                 struct limine_smp_info *info_struct = &ret[*cpu_count];
 
-                info_struct->acpi_processor_uid = gicc->acpi_uid;
-                info_struct->gic_iface_no       = gicc->iface_no;
-                info_struct->mpidr              = gicc->mpidr;
+                info_struct->processor_id = gicc->acpi_uid;
+                info_struct->gic_iface_no = gicc->iface_no;
+                info_struct->mpidr = gicc->mpidr;
 
                 // Do not try to restart the BSP
                 if (gicc->mpidr == bsp_mpidr) {

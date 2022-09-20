limine-bootloader/limine

commit 3181293a044627603c0134c7a48f113248aefe1c

misc: Improve printing effectively on panics and errors

common/lib/panic.s2.c
@@ -21,45 +21,11 @@ noreturn void panic(bool allow_menu, const char *fmt, ...) {
 
     quiet = false;
 
-    static bool is_nested = false;
-
-    if (is_nested) {
-        goto nested;
-    }
-
-    is_nested = true;
-
-#if defined (BIOS)
-    if (stage3_loaded == true && term_backend == NOT_READY) {
-        early_term = true;
-        term_vbe(NULL, 0, 0);
-    }
-#endif
-
-#if defined (UEFI)
-    if (term_backend == NOT_READY) {
-        if (term_enabled_once) {
-            term_vbe(NULL, 0, 0);
-        } else {
-            term_fallback();
-        }
-    }
-#endif
-
-nested:
-    if (term_backend == NOT_READY) {
-        term_fallback();
-    }
-
-#if defined (BIOS)
-    if (stage3_loaded) {
-#endif
+    if (term_backend != FALLBACK) {
         print("\033[31mPANIC\033[37;1m\033[0m: ");
-#if defined (BIOS)
     } else {
         print("PANIC: ");
     }
-#endif
     vprint(fmt, args);
 
     va_end(args);

common/lib/print.s2.c
@@ -11,29 +11,6 @@
 #include <sys/cpu.h>
 #include <drivers/serial.h>
 
-#if defined (BIOS)
-static void s2_print(const char *s, size_t len) {
-    for (size_t i = 0; i < len; i++) {
-        struct rm_regs r = {0};
-        char c = s[i];
-
-        switch (c) {
-            case '\n':
-                r.eax = 0x0e00 | '\r';
-                rm_int(0x10, &r, &r);
-                r = (struct rm_regs){0};
-                r.eax = 0x0e00 | '\n';
-                rm_int(0x10, &r, &r);
-                break;
-            default:
-                r.eax = 0x0e00 | s[i];
-                rm_int(0x10, &r, &r);
-                break;
-        }
-    }
-}
-#endif
-
 static const char *base_digits = "0123456789abcdef";
 
 #define PRINT_BUF_MAX 4096
@@ -142,6 +119,10 @@ void print(const char *fmt, ...) {
 static char print_buf[PRINT_BUF_MAX];
 
 void vprint(const char *fmt, va_list args) {
+    if (quiet) {
+        return;
+    }
+
     size_t print_buf_i = 0;
 
     for (;;) {
@@ -199,15 +180,7 @@ void vprint(const char *fmt, va_list args) {
     }
 
 out:
-#if defined (BIOS)
-    if (stage3_loaded) {
-#endif
-        term_write((uint64_t)(uintptr_t)print_buf, print_buf_i);
-#if defined (BIOS)
-    } else {
-        s2_print(print_buf, print_buf_i);
-    }
-#endif
+    term_write((uint64_t)(uintptr_t)print_buf, print_buf_i);
 
     for (size_t i = 0; i < print_buf_i; i++) {
 #if defined (__x86_64__) || defined (__i386__)
@@ -215,7 +188,7 @@ out:
             outb(0xe9, print_buf[i]);
         }
 #endif
-        if ((term_backend != NOT_READY && serial) || COM_OUTPUT) {
+        if (serial || COM_OUTPUT) {
             switch (print_buf[i]) {
                 case '\n':
                     serial_out('\r');

common/lib/term.c
@@ -52,7 +52,7 @@ void term_deinit(void) {
             gterm_deinit();
     }
 
-    term_notready();
+    term_fallback();
 }
 
 void term_vbe(char *config, size_t width, size_t height) {
@@ -108,8 +108,6 @@ void term_vbe(char *config, size_t width, size_t height) {
     term_context_save = gterm_context_save;
     term_context_restore = gterm_context_restore;
     term_full_refresh = gterm_full_refresh;
-
-    term_enabled_once = true;
 
     term_backend = VBE;
 }
@@ -183,8 +181,6 @@ void term_textmode(void) {
         return;
     }
 
-    term_notready();
-
     init_vga_textmode(&term_rows, &term_cols, true);
 
     if (serial) {
@@ -222,8 +218,6 @@ void term_textmode(void) {
     term_context_restore = text_context_restore;
     term_full_refresh = text_full_refresh;
 
-    term_enabled_once = true;
-
     term_backend = TEXTMODE;
 }
 #endif
@@ -260,9 +254,6 @@ static uint8_t xfer_buf[TERM_XFER_CHUNK];
 bool term_autoflush = true;
 
 void term_write(uint64_t buf, uint64_t count) {
-    if (term_backend == NOT_READY)
-        return;
-
     switch (count) {
         case TERM_CTX_SIZE: {
             uint64_t ret = context_size();

common/lib/term.h
@@ -39,7 +39,7 @@ extern struct term_context {
 } term_context;
 
 enum {
-    NOT_READY,
+    _NOT_READY,
     VBE,
     TEXTMODE,
     FALLBACK
@@ -49,8 +49,6 @@ extern int current_video_mode;
 extern int term_backend;
 extern size_t term_rows, term_cols;
 extern bool term_runtime;
-extern bool early_term;
-extern bool term_enabled_once;
 
 void term_fallback(void);
 
@@ -58,7 +56,6 @@ void term_reinit(void);
 void term_deinit(void);
 void term_vbe(char *config, size_t width, size_t height);
 void term_textmode(void);
-void term_notready(void);
 void term_putchar(uint8_t c);
 void term_write(uint64_t buf, uint64_t count);
 

common/lib/term.s2.c
@@ -5,13 +5,10 @@
 #include <lib/misc.h>
 #include <lib/real.h>
 
-bool early_term = false;
-
 no_unwind int current_video_mode = -1;
-int term_backend = NOT_READY;
+int term_backend = _NOT_READY;
 size_t term_rows, term_cols;
 bool term_runtime = false;
-bool term_enabled_once = false;
 
 void (*raw_putchar)(uint8_t c);
 void (*clear)(bool move);
@@ -139,6 +136,8 @@ static void fallback_get_cursor_pos(size_t *x, size_t *y) {
 }
 #endif
 
+static void term_notready(void);
+
 void term_fallback(void) {
 #if defined (UEFI)
     if (!efi_boot_services_exited) {
@@ -194,9 +193,7 @@ static void notready_uint64_t(uint64_t n) {
     (void)n;
 }
 
-void term_notready(void) {
-    term_backend = NOT_READY;
-
+static void term_notready(void) {
     raw_putchar = notready_raw_putchar;
     clear = notready_clear;
     enable_cursor = notready_void;

common/protos/limine.c
@@ -738,6 +738,7 @@ FEAT_START
 FEAT_END
 
     term_deinit();
+    quiet = true;
 
     if (!fb_init(&fb, req_width, req_height, req_bpp)) {
         panic(true, "limine: Could not acquire framebuffer");

common/protos/linux.c
@@ -496,6 +496,7 @@ noreturn void linux_load(char *config, char *cmdline) {
     ///////////////////////////////////////
 
     term_deinit();
+    quiet = true;
 
     struct screen_info *screen_info = &boot_params->screen_info;
 

common/protos/multiboot1.c
@@ -303,6 +303,7 @@ noreturn void multiboot1_load(char *config, char *cmdline) {
     multiboot1_info->flags |= (1 << 9);
 
     term_deinit();
+    quiet = true;
 
     if (header.flags & (1 << 2)) {
         size_t req_width  = header.fb_width;

commit a6189abdc1f6061009b0d1065fcd97075e6a8824

misc: Fix bugs introduced in 3181293a

common/lib/panic.s2.c
@@ -21,6 +21,10 @@ noreturn void panic(bool allow_menu, const char *fmt, ...) {
 
     quiet = false;
 
+    if (term_backend == _NOT_READY) {
+        term_fallback();
+    }
+
     if (term_backend != FALLBACK) {
         print("\033[31mPANIC\033[37;1m\033[0m: ");
     } else {

common/lib/print.s2.c
@@ -142,10 +142,6 @@ void print(const char *fmt, ...) {
 static char print_buf[PRINT_BUF_MAX];
 
 void vprint(const char *fmt, va_list args) {
-    if (quiet) {
-        return;
-    }
-
     size_t print_buf_i = 0;
 
     for (;;) {
@@ -222,15 +218,17 @@ void vprint(const char *fmt, va_list args) {
     }
 
 out:
+    if (!quiet) {
 #if defined (BIOS)
-    if (stage3_loaded) {
+        if (stage3_loaded) {
 #endif
-        term_write((uint64_t)(uintptr_t)print_buf, print_buf_i);
+            term_write((uint64_t)(uintptr_t)print_buf, print_buf_i);
 #if defined (BIOS)
-    } else {
-        s2_print(print_buf, print_buf_i);
+        } else {
+            s2_print(print_buf, print_buf_i);
+        }
+#endif
     }
-#endif
 
     for (size_t i = 0; i < print_buf_i; i++) {
 #if defined (__x86_64__) || defined (__i386__)
@@ -238,7 +236,7 @@ out:
             outb(0xe9, print_buf[i]);
         }
 #endif
-        if (serial || COM_OUTPUT) {
+        if ((!quiet && serial) || COM_OUTPUT) {
             switch (print_buf[i]) {
                 case '\n':
                     serial_out('\r');

common/lib/term.c
@@ -52,7 +52,7 @@ void term_deinit(void) {
             gterm_deinit();
     }
 
-    term_fallback();
+    term_notready();
 }
 
 void term_vbe(char *config, size_t width, size_t height) {
@@ -252,6 +252,8 @@ static uint8_t xfer_buf[TERM_XFER_CHUNK];
 #endif
 
 bool term_autoflush = true;
+
+static void term_putchar(uint8_t c);
 
 void term_write(uint64_t buf, uint64_t count) {
     switch (count) {
@@ -1026,7 +1028,7 @@ static uint8_t dec_special_to_cp437(uint8_t c) {
     return c;
 }
 
-void term_putchar(uint8_t c) {
+static void term_putchar(uint8_t c) {
     if (discard_next || (term_runtime == true && (c == 0x18 || c == 0x1a))) {
         discard_next = false;
         escape = false;

common/lib/term.h
@@ -50,13 +50,13 @@ extern int term_backend;
 extern size_t term_rows, term_cols;
 extern bool term_runtime;
 
+void term_notready(void);
 void term_fallback(void);
 
 void term_reinit(void);
 void term_deinit(void);
 void term_vbe(char *config, size_t width, size_t height);
 void term_textmode(void);
-void term_putchar(uint8_t c);
 void term_write(uint64_t buf, uint64_t count);
 
 extern void (*raw_putchar)(uint8_t c);

common/lib/term.s2.c
@@ -136,8 +136,6 @@ static void fallback_get_cursor_pos(size_t *x, size_t *y) {
 }
 #endif
 
-static void term_notready(void);
-
 void term_fallback(void) {
 #if defined (UEFI)
     if (!efi_boot_services_exited) {
@@ -193,7 +191,7 @@ static void notready_uint64_t(uint64_t n) {
     (void)n;
 }
 
-static void term_notready(void) {
+void term_notready(void) {
     raw_putchar = notready_raw_putchar;
     clear = notready_clear;
     enable_cursor = notready_void;
@@ -224,4 +222,6 @@ static void term_notready(void) {
 
     term_cols = 80;
     term_rows = 24;
+
+    term_backend = _NOT_READY;
 }

common/protos/limine.c
@@ -738,7 +738,6 @@ FEAT_START
 FEAT_END
 
     term_deinit();
-    quiet = true;
 
     if (!fb_init(&fb, req_width, req_height, req_bpp)) {
         panic(true, "limine: Could not acquire framebuffer");

common/protos/linux.c
@@ -496,7 +496,6 @@ noreturn void linux_load(char *config, char *cmdline) {
     ///////////////////////////////////////
 
     term_deinit();
-    quiet = true;
 
     struct screen_info *screen_info = &boot_params->screen_info;
 

common/protos/multiboot1.c
@@ -303,7 +303,6 @@ noreturn void multiboot1_load(char *config, char *cmdline) {
     multiboot1_info->flags |= (1 << 9);
 
     term_deinit();
-    quiet = true;
 
     if (header.flags & (1 << 2)) {
         size_t req_width  = header.fb_width;

common/protos/multiboot2.c
@@ -498,7 +498,6 @@ noreturn void multiboot2_load(char *config, char* cmdline) {
         tag->common.size = sizeof(struct multiboot_tag_framebuffer);
 
         term_deinit();
-        quiet = true;
 
         if (fbtag) {
             size_t req_width = fbtag->width;
b58d7b13e3ecb9f1afe58ecde674574e7b5034f9
mintsuki <mintsuki@protonmail.com>
2022-10-04 20:39:50 +0200

    term: Reimplement context control

--- common/drivers/vga_textmode.h
+++ common/drivers/vga_textmode.h
@@ -3,7 +3,30 @@
 
 #if defined (BIOS)
 
+#include <stdint.h>
+#include <stddef.h>
 #include <stdbool.h>
+#include <term/term.h>
+
+#define VD_COLS (80 * 2)
+#define VD_ROWS 25
+
+struct textmode_context {
+    struct term_context term;
+
+    volatile uint8_t *video_mem;
+
+    uint8_t *back_buffer;
+    uint8_t *front_buffer;
+
+    size_t cursor_offset;
+    size_t old_cursor_offset;
+    bool cursor_status;
+    uint8_t text_palette;
+
+    uint8_t saved_state_text_palette;
+    size_t saved_state_cursor_offset;
+};
 
 void vga_textmode_init(bool managed);
 

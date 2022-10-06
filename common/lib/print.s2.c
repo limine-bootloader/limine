ff145eb5118fd7b58e0b0223b188f113fa77bfa0
mintsuki <mintsuki@protonmail.com>
2022-10-06 05:29:13 +0200

    term: Misc improvements for SERIAL=yes on EFI

--- common/lib/print.s2.c
+++ common/lib/print.s2.c
@@ -219,6 +219,7 @@ out:
             outb(0xe9, print_buf[i]);
         }
 #endif
+#if defined (BIOS)
         if ((!quiet && serial) || COM_OUTPUT) {
             switch (print_buf[i]) {
                 case '\n':
@@ -234,5 +235,6 @@ out:
             }
             serial_out(print_buf[i]);
         }
+#endif
     }
 }

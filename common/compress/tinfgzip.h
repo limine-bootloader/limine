a3f814b5f75c30d85bdc5d9615f5570b91ff3a39
mintsuki <mintsuki@protonmail.com>
2023-02-16 02:05:11 +0100

    uri/tinfgzip: Minor fixes and layering improvement

--- common/compress/tinfgzip.h
+++ common/compress/tinfgzip.h
@@ -1,7 +1,8 @@
 #ifndef __COMPRESS__TINFGZIP_H__
 #define __COMPRESS__TINFGZIP_H__
 
-int tinf_gzip_uncompress(void *dest, unsigned int limit,
-                         const void *source, unsigned int sourceLen);
+#include <stdint.h>
+
+void *tinf_gzip_uncompress(const void *source, uint64_t sourceLen, uint64_t *outsize);
 
 #endif

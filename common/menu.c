limine-bootloader/limine

commit 48053fd206dac61d38e5dde85dfd1f8d5849916c

misc: Fix bug on trunk where returns to menu from panic would crash

common/menu.c
@@ -625,10 +625,6 @@ noreturn void _menu(bool first_run) {
 #endif
     }
 
-    if (!first_run) {
-        term_fallback();
-    }
-
     if (bad_config == false) {
 #if defined (UEFI)
         if (init_config_disk(boot_volume)) {

common/entry.s3.c
@@ -130,5 +130,7 @@ noreturn void stage3_common(void) {
     init_io_apics();
 #endif
 
+    term_notready();
+
     menu(true);
 }

common/lib/panic.s2.c
@@ -54,6 +54,9 @@ noreturn void panic(bool allow_menu, const char *fmt, ...) {
 
         getchar();
 
+        // This fixes a crash
+        term_notready();
+
         menu(false);
 /*
         fb_clear(&fbinfo);

common/linker_bios.ld.in
@@ -43,6 +43,7 @@ SECTIONS
         term_write = .;
         term_backend = .;
         term_fallback = .;
+        term_notready = .;
         terms = .;
         terms_i = .;
         stage3_addr = .;

commit c7f344784c54fc07ecd2672d64383cece855e7c5

menu: Fix issue introduced in 48053fd2 where TIMEOUT=0 would not show interface

common/menu.c
@@ -625,6 +625,8 @@ noreturn void _menu(bool first_run) {
 #endif
     }
 
+    term_fallback();
+
     if (bad_config == false) {
 #if defined (UEFI)
         if (init_config_disk(boot_volume)) {
4ef529b4eb99f866f8a130db2c25259a4d201a60
mintsuki <mintsuki@protonmail.com>
2023-03-07 02:21:09 +0100

    misc: Load BIOS port at 0xf000 instead of 0x8000

--- decompressor/main.c
+++ decompressor/main.c
@@ -4,18 +4,18 @@
 #include <tinf.h>
 
 noreturn void entry(uint8_t *compressed_stage2, size_t stage2_size, uint8_t boot_drive, int pxe) {
-    // The decompressor should decompress compressed_stage2 to address 0x8000.
-    uint8_t *dest = (uint8_t *)0x8000;
+    // The decompressor should decompress compressed_stage2 to address 0xf000.
+    uint8_t *dest = (uint8_t *)0xf000;
 
     tinf_gzip_uncompress(dest, compressed_stage2, stage2_size);
 
     asm volatile (
-        "movl $0x7c00, %%esp\n\t"
+        "movl $0xf000, %%esp\n\t"
         "xorl %%ebp, %%ebp\n\t"
         "pushl %1\n\t"
         "pushl %0\n\t"
         "pushl $0\n\t"
-        "pushl $0x8000\n\t"
+        "pushl $0xf000\n\t"
         "ret\n\t"
         :
         : "r" ((uint32_t)boot_drive), "r" (pxe)

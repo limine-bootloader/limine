94a19a5c3717b6a5a00df938b3cf312ae4066031
mintsuki <mintsuki@protonmail.com>
2023-07-10 18:57:06 +0200

    host/limine: Fix typo

--- host/limine.c
+++ host/limine.c
@@ -1002,7 +1002,7 @@ uninstall_mode_cleanup:
 static void enroll_config_usage(const char *name) {
     printf("Usage: %s enroll-config <Limine executable> <BLAKE2B of config file>\n", name);
     printf("\n");
-    printf("    --reset      Remove enrolled BLAKE2B, will not check config intergrity\n");
+    printf("    --reset      Remove enrolled BLAKE2B, will not check config integrity\n");
     printf("\n");
     printf("    --quiet      Do not print verbose diagnostic messages\n");
     printf("\n");

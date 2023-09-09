b1c5f561d4cc0f06c652ad1f689bd57280b90517
mintsuki <mintsuki@protonmail.com>
2023-09-09 09:45:47 -0500

    stage1/gdt: Mark accessed flag in descriptors

--- stage1/gdt.asm
+++ stage1/gdt.asm
@@ -7,7 +7,7 @@ gdt:
     dw 0xffff       ; Limit
     dw 0x0000       ; Base (low 16 bits)
     db 0x00         ; Base (mid 8 bits)
-    db 10011010b    ; Access
+    db 10011011b    ; Access
     db 11001111b    ; Granularity
     db 0x00         ; Base (high 8 bits)
 
@@ -15,7 +15,7 @@ gdt:
     dw 0xffff       ; Limit
     dw 0x0000       ; Base (low 16 bits)
     db 0x00         ; Base (mid 8 bits)
-    db 10010010b    ; Access
+    db 10010011b    ; Access
     db 11001111b    ; Granularity
     db 0x00         ; Base (high 8 bits)
 

749a32066cd4aaf03ca6e352a918dc4992b9fa5c
mintsuki <mintsuki@users.noreply.github.com>
2024-01-26 00:35:08 +0000

    Binary release v7.0.2

--- limine.c
+++ limine.c
@@ -1163,7 +1163,7 @@ cleanup:
     return ret;
 }
 
-#define LIMINE_VERSION "7.0.1"
+#define LIMINE_VERSION "7.0.2"
 #define LIMINE_COPYRIGHT "Copyright (C) 2019-2024 mintsuki and contributors."
 
 static void version_usage(void) {

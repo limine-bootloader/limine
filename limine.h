81bc80900f42e67b32e373a193653fcdf0247312
mintsuki <mintsuki@users.noreply.github.com>
2024-05-03 18:57:15 +0000

    Binary release v7.5.0

--- limine.h
+++ limine.h
@@ -44,8 +44,13 @@ extern "C" {
 #  define LIMINE_DEPRECATED_IGNORE_END
 #endif
 
-#define LIMINE_REQUESTS_DELIMITER \
-    uint64_t limine_requests_delimiter[2] = { 0xadc0e0531bb10d03, 0x9572709f31764c62 };
+#define LIMINE_REQUESTS_START_MARKER \
+    uint64_t limine_requests_start_marker[4] = { 0xf6b8f4b39de7d1ae, 0xfab91a6940fcb9cf, \
+                                                 0x785c6ed015d3e316, 0x181e920a7852b9d9 };
+#define LIMINE_REQUESTS_END_MARKER \
+    uint64_t limine_requests_end_marker[2] = { 0xadc0e0531bb10d03, 0x9572709f31764c62 };
+
+#define LIMINE_REQUESTS_DELIMITER LIMINE_REQUESTS_END_MARKER
 
 #define LIMINE_BASE_REVISION(N) \
     uint64_t limine_base_revision[3] = { 0xf9562b2d5c95a6c8, 0x6a7b384944536bdc, (N) };

limine-bootloader/limine

commit cc05481b89f13c842645d5cacc78908d6a9f102e

dtb: Fix bugs introduced in 45e036275af9867471e141444f7d07db97f5d0e9

common/lib/misc.c
@@ -112,6 +112,8 @@ uint32_t hex2bin(uint8_t *str, uint32_t size) {
 #if defined (UEFI)
 
 void *get_device_tree_blob(size_t extra_size) {
+    int ret;
+
     EFI_GUID dtb_guid = EFI_DTB_TABLE_GUID;
     for (size_t i = 0; i < gST->NumberOfTableEntries; i++) {
         EFI_CONFIGURATION_TABLE *cur_table = &gST->ConfigurationTable[i];
@@ -122,9 +124,15 @@ void *get_device_tree_blob(size_t extra_size) {
 
         printv("efi: found dtb at %p, size %x\n", cur_table->VendorTable, s);
 
+        if (extra_size == (size_t)-1) {
+            extra_size = 0;
+        }
         void *new_tab = ext_mem_alloc(s + extra_size);
 
-        fdt_resize(cur_table->VendorTable, new_tab, s + extra_size);
+        ret = fdt_resize(cur_table->VendorTable, new_tab, s + extra_size);
+        if (ret < 0) {
+            panic(true, "dtb: failed to resize new DTB");
+        }
 
         return new_tab;
     }
@@ -134,8 +142,6 @@ void *get_device_tree_blob(size_t extra_size) {
     }
 
     void *dtb = ext_mem_alloc(extra_size);
-
-    int ret;
 
     ret = fdt_create_empty_tree(dtb, extra_size);
     if (ret < 0) {

commit 68ad2db75527321c8f9dfe9958f9559a395184ad

dtb: Fix bugs introduced in cc05481b89f13c842645d5cacc78908d6a9f102e

common/lib/misc.c
@@ -124,9 +124,10 @@ void *get_device_tree_blob(size_t extra_size) {
 
         printv("efi: found dtb at %p, size %x\n", cur_table->VendorTable, s);
 
-        if (extra_size == (size_t)-1) {
-            extra_size = 0;
+        if (extra_size == 0) {
+            return cur_table->VendorTable;
         }
+
         void *new_tab = ext_mem_alloc(s + extra_size);
 
         ret = fdt_resize(cur_table->VendorTable, new_tab, s + extra_size);
@@ -137,7 +138,7 @@ void *get_device_tree_blob(size_t extra_size) {
         return new_tab;
     }
 
-    if (extra_size == (size_t)-1) {
+    if (extra_size == 0) {
         return NULL;
     }
 

common/sys/cpu_riscv.c
@@ -225,7 +225,7 @@ static void init_riscv_fdt(const void *fdt) {
 }
 
 void init_riscv(void) {
-    void *fdt = get_device_tree_blob((size_t)-1);
+    void *fdt = get_device_tree_blob(0);
     if (fdt != NULL) {
         init_riscv_fdt(fdt);
     } else if (acpi_get_rsdp()) {
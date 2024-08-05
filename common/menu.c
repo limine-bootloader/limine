limine-bootloader/limine

commit a30cad355604c5b80d5665c585456f175e98bf7d

menu: Change menu arrow to use unicode big arrow

common/menu.c
@@ -548,12 +548,14 @@ static size_t print_tree(size_t offset, size_t window, const char *shift, size_t
         }
         if (current_entry->sub) {
             if (!no_print) print(current_entry->expanded ? "[-]" : "[+]");
+            cur_len += 3;
         } else if (level) {
-            if (!no_print) print(serial ? "-> " : "─> ");
+            if (!no_print) print(serial ? "-> " : "─►");
+            cur_len += 2;
         } else {
             if (!no_print) print("   ");
+            cur_len += 3;
         }
-        cur_len += 3;
         if (base_index + max_entries == selected_entry) {
             *selected_menu_entry = current_entry;
             if (!no_print) print("\e[7m");

commit 7489ef1f6fc3cb94bb64bbda02ff0e5a6954dee6

menu: Fix alignment bug introduced in a30cad355. Fixes #396

common/menu.c
@@ -560,14 +560,12 @@ static size_t print_tree(size_t offset, size_t window, const char *shift, size_t
         }
         if (current_entry->sub) {
             if (!no_print) print(current_entry->expanded ? "[-]" : "[+]");
-            cur_len += 3;
         } else if (level) {
-            if (!no_print) print(serial ? "-> " : "─►");
-            cur_len += 2;
+            if (!no_print) print(serial ? "-> " : "─► ");
         } else {
             if (!no_print) print("   ");
-            cur_len += 3;
         }
+        cur_len += 3;
         if (base_index + max_entries == selected_entry) {
             *selected_menu_entry = current_entry;
             if (!no_print) print("\e[7m");